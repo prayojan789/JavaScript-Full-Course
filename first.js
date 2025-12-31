8928b04858f14fcdb206ee5f71369526f631458d
Prayojan Puri <pprayojan888@gmail.com>
2025-12-31 20:07:49 +0545

    Basics of JavaScript

---
+++ first.js
@@ -0,0 +1,92 @@
+// console.log("Prayojan Puri");
+// console.log("Welcome to my Website");
+// console.log("I am learning JavaScript from youtube");
+
+// fullName = "Prayojan Puri";
+// console.log("fullName");
+  age =24;
+  price = 99.99;
+  x= null;
+  y = undefined;
+  console.log(x);
+  isFollow = true;
+
+// fullName = 25;
+
+//   console.log(fullName);
+
+
+//   fullname = "Prayojan Puri";
+//   fullName = "Puri Prayojan";
+
+//   console.log(fullName);    
+//   console.log(fullname);
+
+    // fullname = "Prayojan Puri";
+    // _fullname = "Prayojan Puri";
+    // $fullname = "Prayojan Puri";
+    // fullname = "Prayojan Puri";
+
+    // isFollow
+    // totalPrice
+
+    var fullName = "Prayojan Puri";
+    var age = 23;
+    var totalPrice = 10000;
+
+    console.log(fullName);
+
+    var age = 24;
+    var age = 59;
+    var age = 86;
+
+    console.log(age);
+
+    // const PI = 3.14;
+    //  console.log(PI);
+
+    //  let a ;
+    //  console.log(a);    
+
+    //  const a;
+    //  console.log(a);
+    {
+        let a = 5;
+        console.log(a);
+    }
+    {
+        let a = 10;
+        console.log(a);
+    }
+
+    const student = {
+        fullName: "Prayojan Puri",
+        age: 23,
+        gpa:3,
+        isPass : true,
+    };
+
+    student["age"]  =student ["age"]+1;
+    student["name"] = "Puri Prayojan";
+
+    console.log(student.age);
+    console.log(student.gpa);
+    console.log(student.name);
+
+    const product = {
+        title : "Ball Pen",
+        rating :4,
+        offer :5,
+        price : 270,
+    };
+
+    console.log(product);
+
+    const profile = {
+        username : "@prayojanpuri",
+        isFollow :false,
+        followers: 345,
+        following:280,
+    };
+
+    console.log(typeof profile.username);   
