1c770139b42f5e6a817afa924f17e75e08adf5a1
Prayojan Puri <pprayojan888@gmail.com>
2026-01-01 21:02:40 +0545

    Operators and conditional statements in JavaScript

---
+++ scripts.js
@@ -0,0 +1,57 @@
+//this is a code that prints 
+//Hello world on our console windows
+
+// console.log("Hello World");
+
+//Artimetic Operators
+//  let a= 5 ;
+//  let b= 6 ;
+
+//  console.log("a = ",a,"&b =",b); 
+//  console.log("a+b=", a+b);
+//  console.log("a-b =", a-b);
+//  console.log("a*b =", a*b);
+//  console.log("a/b =", a/b);
+//  console.log("a%b =", a%b);
+
+ //Unary Operator
+
+//  let a = 5 ;
+//  let b = 6 ;
+
+//  console.log("a = ",a, "& b =", b );
+//  a = a + 1 ; 
+//  console.log("a = ", a) ;
+
+// console.log("a = ", a, "& b = ", b);
+
+// console.log("a --", a --);
+// console.log("a =", a);
+
+
+//Asignment  Operator
+//  let a = 5 ;
+//  let b = 2;
+
+
+//  a **= 4 ; // a= a **4
+//  console.log("a =", a);
+
+ //Comparison Operators
+
+//  let a = 5 ; 
+//  let b = 2;
+
+//  console.log("5 == 2", a == b); // false
+//  console.log("5 != 2", a != b); // false
+//  console.log("5 == 2", a == b); // false
+
+//  let a = 5 ; //number 
+//  let b = "5";// string
+
+
+//  console.log("a == b", a == b); // true
+ 
+
+//Logical Operators
+
