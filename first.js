142d84f0b8d38074e6048fe74842333004ab6732
Prayojan Puri <pprayojan888@gmail.com>
2026-01-02 20:13:05 +0545

    Logical Operators in JavaScripts

--- first.js
+++ first.js
@@ -4,89 +4,89 @@
 
 // fullName = "Prayojan Puri";
 // console.log("fullName");
-  age =24;
-  price = 99.99;
-  x= null;
-  y = undefined;
-  console.log(x);
-  isFollow = true;
+//   age =24;
+//   price = 99.99;
+//   x= null;
+//   y = undefined;
+//   console.log(x);
+//   isFollow = true;
 
-// fullName = 25;
+// // fullName = 25;
 
-//   console.log(fullName);
+// //   console.log(fullName);
 
 
-//   fullname = "Prayojan Puri";
-//   fullName = "Puri Prayojan";
+// //   fullname = "Prayojan Puri";
+// //   fullName = "Puri Prayojan";
 
-//   console.log(fullName);    
-//   console.log(fullname);
+// //   console.log(fullName);    
+// //   console.log(fullname);
 
-    // fullname = "Prayojan Puri";
-    // _fullname = "Prayojan Puri";
-    // $fullname = "Prayojan Puri";
-    // fullname = "Prayojan Puri";
+//     // fullname = "Prayojan Puri";
+//     // _fullname = "Prayojan Puri";
+//     // $fullname = "Prayojan Puri";
+//     // fullname = "Prayojan Puri";
 
-    // isFollow
-    // totalPrice
+//     // isFollow
+//     // totalPrice
 
-    var fullName = "Prayojan Puri";
-    var age = 23;
-    var totalPrice = 10000;
+//     var fullName = "Prayojan Puri";
+//     var age = 23;
+//     var totalPrice = 10000;
 
-    console.log(fullName);
+//     console.log(fullName);
 
-    var age = 24;
-    var age = 59;
-    var age = 86;
+//     var age = 24;
+//     var age = 59;
+//     var age = 86;
 
-    console.log(age);
+//     console.log(age);
 
-    // const PI = 3.14;
-    //  console.log(PI);
+//     // const PI = 3.14;
+//     //  console.log(PI);
 
-    //  let a ;
-    //  console.log(a);    
+//     //  let a ;
+//     //  console.log(a);    
 
-    //  const a;
-    //  console.log(a);
-    {
-        let a = 5;
-        console.log(a);
-    }
-    {
-        let a = 10;
-        console.log(a);
-    }
+//     //  const a;
+//     //  console.log(a);
+//     {
+//         let a = 5;
+//         console.log(a);
+//     }
+//     {
+//         let a = 10;
+//         console.log(a);
+//     }
 
-    const student = {
-        fullName: "Prayojan Puri",
-        age: 23,
-        gpa:3,
-        isPass : true,
-    };
+//     const student = {
+//         fullName: "Prayojan Puri",
+//         age: 23,
+//         gpa:3,
+//         isPass : true,
+//     };
 
-    student["age"]  =student ["age"]+1;
-    student["name"] = "Puri Prayojan";
+//     student["age"]  =student ["age"]+1;
+//     student["name"] = "Puri Prayojan";
 
-    console.log(student.age);
-    console.log(student.gpa);
-    console.log(student.name);
+//     console.log(student.age);
+//     console.log(student.gpa);
+//     console.log(student.name);
 
-    const product = {
-        title : "Ball Pen",
-        rating :4,
-        offer :5,
-        price : 270,
-    };
+//     const product = {
+//         title : "Ball Pen",
+//         rating :4,
+//         offer :5,
+//         price : 270,
+//     };
 
-    console.log(product);
+//     console.log(product);
 
-    const profile = {
-        username : "@prayojanpuri",
-        isFollow :false,
-        followers: 345,
-        following:280,
-    };
+//     const profile = {
+//         username : "@prayojanpuri",
+//         isFollow :false,
+//         followers: 345,
+//         following:280,
+//     };
 
-    console.log(typeof profile.username);   
+//     console.log(typeof profile.username);   
--- scripts.js
+++ scripts.js
@@ -55,3 +55,17 @@
 
 //Logical Operators
 
+let a = 6;
+let b = 5;
+
+
+let  cond1 = a>b ; //true
+let  cond2 = a<b ; //true
+let  cond3 = a===6 ; //true
+
+console.log("cond1 && cond2 =", cond1 && cond2);
+console.log("cond2 && cond3 =", cond2 && cond3);
+console.log("cond1 && cond2 =", a <b && a===6);
+console.log("cond1 && cond2 =", a <b || a===6);
+console.log("!(6<5) =", !( a===6)); 
+
