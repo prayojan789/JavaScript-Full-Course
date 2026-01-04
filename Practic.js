dea51bb13e66e1621a4483aa3673ff576fae5664
Prayojan Puri <pprayojan888@gmail.com>
2026-01-04 23:27:39 +0545

    Basic about Arrays

--- Practic.js
+++ Practic.js
@@ -77,8 +77,42 @@
 //Q5. Prompt the user to enter their full name. Generate a username for them based on the Input. Start username with @, followed by their full name and ending with the fullname length.
 
 
-    let fullName = prompt("Enter your fullname without spaces");
+    // let fullName = prompt("Enter your fullname without spaces");
 
-    let username = "@" + fullName +  fullName.length;
-    console.log(username);
+    // let username = "@" + fullName +  fullName.length;
+    // console.log(username);
     
+
+//Q6. For a given array with marks of students -> [85, 97, 44, 37, 76, 60]
+//Find the average marks of the entire class.
+
+// let marks =[85, 97, 44, 37, 76, 60];
+
+// let sum = 0;
+
+//  for(let val of marks) {
+//     sum += val;
+//  }
+//      let avg = sum / marks.length;
+//      console.log(`avg marks of the class = ${avg}`);
+     
+
+
+//Q6. For a give array with prices of 5items  ->[250, 645, 300, 900, 50]
+// All items have an offer of 10% OFF on them. Change the array to store final Price
+// after applying offer.
+
+let items = [250, 645, 300, 900, 50]
+
+let i = 0;
+for (let val of items) {
+    console.log(`value at index ${i} = ${val}`);
+    let offer = val / 10;
+    items[i] = items[i] - offer;
+    console.log(`value after offer = ${val}`);
+    i++;
+}
+
+// for (let i = 0; i<items.length; i ++){
+
+// }
---
+++ arrays.js
@@ -0,0 +1,33 @@
+// let marks_students1 =97;
+// let marks_students2 =82;
+
+
+// let marks = {
+//     stu1: 97,
+//     stu2: 82,
+// }
+
+// let marks = [97, 82, 75, 64, 36]
+// console.log(marks);
+// console.log(marks.length);
+
+// let heros = ["Prayojan","Prayojan","Prayojan","Prayojan"]
+
+// console.log(heros);
+
+// for (let idx=0; idx<heros.length; idx++) {
+//     console.log(heros[idx]);
+    
+// }
+
+// for of 
+// for (let hero of heros) {
+//     console.log(hero);
+    
+// }
+
+let cities = ["kathmandu", "pokhara", "butwal", "chitwan"];
+ for (let city of cities) {
+    console.log(city.toLocaleUpperCase);
+    
+ }
