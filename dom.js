3920fd8cc8e8994bbab37b880e22d6c32ad15090
Prayojan Puri <pprayojan888@gmail.com>
2026-01-07 20:46:21 +0545

    Concept of DOM (Document Object Model)

--- dom.js
+++ dom.js
@@ -1,6 +1,12 @@
 // console.log("hello!");
 // alert("Prayojan Puri");
 
-console.log("Hello");
-window.console.log("Hello2");
- window.alert("Hello");
+// console.log("Hello");
+// window.console.log("Hello2");
+//  window.alert("Hello");
+
+// console.dir(document.body);
+// console.log(document.body);
+// console.dir(document.body.childNodes[3].inner);
+
+document.body.childNodes[3].inner
