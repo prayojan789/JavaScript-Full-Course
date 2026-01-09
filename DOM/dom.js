a064da06755b43eb74a452b40a500bcb69959f23
Prayojan Puri <pprayojan888@gmail.com>
2026-01-09 17:38:22 +0545

    A comprehensive guide to understanding and mastering the Document Object Model (DOM) in JavaScript.

---
+++ DOM/dom.js
@@ -0,0 +1,88 @@
+// console.log("hello!");
+// alert("Prayojan Puri");
+
+// console.log("Hello");
+// window.console.log("Hello2");
+//  window.alert("Hello");
+
+// console.dir(document.body);
+// console.log(document.body);
+// console.dir(document.body.childNodes[3].inner);
+
+// document.body.childNodes[3].inner
+
+
+// let button = document.getElementById("myId"); //h1
+// console.dir(button);
+
+// let headings = document.getElementsByClassName("heading-class");
+// console.dir(headings);
+// console.log(headings);
+
+
+// let parahs = document.getElementsByTagName("p");
+// console.dir(p)
+
+// let firstEl = document.querySelector("myId"); //1st element 
+// console.dir(firstEl);
+
+// let allEl = document.querySelectorAll(".myClass"); //all elements
+// console.dir(allEl);
+
+// let firstEl = document.querySelector("p"); //1st element
+// console.dir(firstEl);
+
+// console.dir(document.body.firstChild);
+
+// let div = document.querySelector("div");
+// console.dir(div);
+
+// let heading = document.querySelector("h1");
+
+// let div = document.querySelector("div");
+// console.log(div);
+
+// let id = div.getAttribute("id");
+// console.log(id);
+
+// let name = div.getAttribute("name");
+// console.log(name);
+
+
+// let para = document.querySelector("p");
+// console.log(para.getAttribute("class"));                              
+
+// let para = document.querySelector("p");
+// consol.log(para.setAttribute   ("class", "newClass"));
+
+
+//   let dev = document.querySelector("div");
+
+// div.style.backgroundColor = "green";
+// div.style.backgroundColor = "purple";
+// div.style.visibility = "hidden";
+
+// div.style.fontSize = "26px";
+
+//  div.innerText = "Hello";
+
+// let newBtn = document.createElement("button");
+// newBtn.innerText = "click me";
+// console.log(newBtn);
+
+// let div = document.querySelector("div");
+// div.before(newBtn);
+//  div.after(newBtn);
+
+// let p = document.querySelector("p");
+// p.after(newBtn);
+
+let newHeading = document.createElement("h1");
+newHeading.innerHTML = "<i>Hello, My name is Prayojan Puri</i>";
+
+document.querySelector("body").prepend(newHeading);
+
+let para = document.querySelector("p");
+para.remove();
+
+newHeading.remove();
