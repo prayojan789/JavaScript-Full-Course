949ca987fa0d3f274e717634e3d66eb61f00c094
Prayojan Puri <pprayojan888@gmail.com>
2026-01-10 22:30:44 +0545

    initialize rock paper scissors game project

--- Practice.js
+++ Practice.js
@@ -266,8 +266,8 @@ document.querySelector("body").prepend(newBtn);
 
 
 // Q15. Create a <p> tag in html, give it a class & styling. 
-// Now create a new class in CSS and try to append this class to the <p> element
+// Now create a new class in CSS and try to appe'.nd this class to the <p> element
 // Did you notice, how you overwrite the class name when you add a new element
-// Solve this problem using clasList.
+// Solve this problem using classList.
 
 let para = document.querySelector("p");
