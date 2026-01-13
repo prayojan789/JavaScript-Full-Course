046aaef2f9fd3e449af513b3bbc42d60e44a1df1
Prayojan Puri <pprayojan888@gmail.com>
2026-01-13 22:38:30 +0545

    Classes and Objects in JavaScript

--- Classes & Object/app.js
+++ Classes & Object/app.js
@@ -3,21 +3,38 @@
 //     marks : 95.5,
 //     printMarks : function(  ){
 //         console.log("marks =", this.marks);
-        
+
 //     },
 // };
 
 const employee = {
-    calTax(){
+    calTax() {
         console.log("tax rate is 10%");
-        
+
     },
     // calctax2 :function(){
     //      console.log("tax rate is 10%");
-        
+
     // }
 };
 
 const PrayojanPuri = {
     salary: 50000,
 }
+
+const PrayojanPuri2 = {
+    salary: 50000,
+}
+
+const PrayojanPuri3 = {
+    salary: 50000,
+}
+
+const PrayojanPuri4748 = {
+    salary: 50000,
+}  
+
+PrayojanPuri.__proto__ = employee;
+PrayojanPuri2.__proto__ = employee;
+PrayojanPuri3.__proto__ = employee;
+PrayojanPuri4.__proto__ = employee;
