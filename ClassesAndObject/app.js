466b717998aacb887d89a50ee7b94e464a5b4e16
Prayojan Puri <pprayojan888@gmail.com>
2026-01-14 08:49:36 +0545

    Classes and Objects in JavaScript

---
+++ ClassesAndObject/app.js
@@ -0,0 +1,56 @@
+// const student = {
+//     fullName : "Prayojan Puri",
+//     marks : 95.5,
+//     printMarks : function(  ){
+//         console.log("marks =", this.marks);
+
+//     },
+// };
+
+// const employee = {
+//     calTax() {
+//         console.log("tax rate is 10%");
+
+//     },
+    // calctax2 :function(){
+    //      console.log("tax rate is 10%");
+
+    // }
+// };
+
+// const PrayojanPuri = {
+//     salary: 50000,
+//     calcTax(){
+//         console.log("Tax rate is 20%");
+//     }
+// }
+
+// const PrayojanPuri2 = {
+//     salary: 50000,
+// }
+
+// const PrayojanPuri3 = {
+//     salary: 50000,
+// }
+
+// const PrayojanPuri4748 = {
+//     salary: 50000,
+// }  
+
+// PrayojanPuri.__proto__ = employee;
+// PrayojanPuri2.__proto__ = employee;
+// PrayojanPuri3.__proto__ = employee;
+// PrayojanPuri4.__proto__ = employee;
+
+
+//classes
+class ToyotaCar{
+    start(){
+    console.log("start");
+    } 
+    stop(){
+        console.log("stop");
+    }
+}
+
+let fortuner = new ToyotaCar();
