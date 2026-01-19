afdabb93ba6b2644247469ea9f831110f93f0769
Prayojan Puri <pprayojan888@gmail.com>
2026-01-19 23:04:14 +0545

    Implemented a wrapper around the external course API to simplify data access and handling. Includes request helpers, response mapping, and basic error handling.

--- fetchapiwithproject/app.js
+++ fetchapiwithproject/app.js
@@ -5,7 +5,7 @@
 // // let promise = fetch(URL);
 // // console.log(promise);
 
-// //wwwwwwww const getFacts = async () => {
+// // const getFacts = async () => {
 // //     console.log("getting data ......");
 // //     let response = await fetch(URL);
 // //     console.log(response);   //JSON format
@@ -23,10 +23,59 @@
 //     .then((data) => {
 //         console.log(data);
 //         factPara.innerText = data[2].text;
-        
+
 //     });
 // }
 
 // btn.addEventListener("click", getFacts);
 
 
+//currency
+
+const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("from button");
+// let (code in countryList){
+//     console.log(code);
+
+// };
+
+for (let select of dropdowns) {
+    for (code in countryList) {
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        if (select.name === "from" && currCode === "USD") {
+            newOption.selected = "selected";
+        } else if (select.name === "to" && currCode === "NR") {
+            newOption.selected = "selected";
+        }
+        select.appendChild(newOption);
+    }
+    select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
+    })
+
+}
+
+const updateFlag = () => {
+    let currCode = element.value;
+    // console.log(currCode);
+    let countryCode = countryList[currCode];
+     let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+     let img = element.parentElement.querySelector("img");
+     img.src = newSrc;
+  
+}
+
+btn.addEventListener("click", (evt) =>{
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input ");
+    let amtVal = amount.value;
+    if(amtVal === "" || amtVal < 1 ) {
+        amtVal = 1;
+        amount.value = "1";
+    }
+
+});
